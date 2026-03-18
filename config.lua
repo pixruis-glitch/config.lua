8b33de60f8c2b35e5ecb80089600269f2f19d6ee
Ruping <260846303+pixruis-glitch@users.noreply.github.com>
2026-03-19 00:41:33 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -86,7 +86,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Auto Use"] = true,
         ["Minimum Rod"] = "Ghostfinn Rod",
         ["Buy List"] = {
-            ["Mutation Totem"] = 10,
+            ["Mutation Totem"] = 50,
             ["Luck Totem"] = 2,
             "Mutation Totem",
             "Luck Totem",
