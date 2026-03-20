8b2c2c0a89e2bb701e91de7552fa005132273eaf
Ruping <260846303+pixruis-glitch@users.noreply.github.com>
2026-03-20 15:29:47 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -146,7 +146,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Location Rods"] = {
             ["Fisherman Island"] = {"Starter Rod"},
             ["Tropical Grove"] = {"Grass Rod", "Midnight Rod"},
-            ["Ancient Ruin"] = {"Element Rod", "Ghostfinn Rod", "Ares Rod", "Astral Rod"},
+            ["Crystal Depths"] = {"Element Rod", "Ghostfinn Rod", "Ares Rod", "Astral Rod"},
         },
         ["Endgame"] = "",
     },
